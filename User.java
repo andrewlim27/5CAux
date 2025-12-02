65d5c443b4fca2306931ab7019ff3b0a9a8a14af
Andrew Lim <andrew.q.lim@gmail.com>
2025-12-01 18:43:58 -0800

    updated user class

--- User.java
+++ User.java
@@ -1,62 +1,35 @@
 public class User {
-    private int id;
-    private String username;
-    private int school;
-    private int year;
-    private String major;
-    static int num_users;
+    private final int userId;
+    private final String name;
+    private final String school;
+    private final int year;
+    private final String major;
 
-    public User(String username, int school, int year){
-        num_users++;
-        id = num_users;
-        this.username = username;
-        this.school = school;
-        this.year = year;
-        major = null;
-    }
-
-    public User(String username, int school, int year, String major){
-        num_users++;
-        id = num_users;
-        this.username = username;
+    public User(int userId, String name, String school, int year, String major){
+        this.userId = userId;
+        this.name = name;
         this.school = school;
         this.year = year;
         this.major = major;
     }
 
-    public int getId(){
-        return id;
+    public int getUserId(){
+        return userId;
     }
 
-    public String getUsername(){
-        return username;
+    public String getName(){
+        return name;
     }
 
-    public void setUsername(String username){
-        this.username = username;
-    }
-
-    public int getSchool(){
+    public String getSchool(){
         return school;
     }
-
-    public void setSchool(int school){
-        this.school = school;
-    }
-
+    
     public int getYear(){
         return year;
     }
-
-    public void setYear(int year){
-        this.year = year;
-    }
-
+    
     public String getMajor(){
         return major;
-    }
-
-    public void setMajor(String major){
-        this.major = major;
     }
 }
